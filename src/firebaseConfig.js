a7deb064a7f8421bc106c43c33a091bf64799b40
Yashaswini2005 <yashaswinigodachi@gmail.com>
2024-11-12 10:15:46 +0530

    project added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDniXX1qL8LSeYuWaqfA51GQ6jyDJmeB5M",
+  authDomain: "project-y-07.firebaseapp.com",
+  databaseURL: "https://project-y-07-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "project-y-07",
+  storageBucket: "project-y-07.firebasestorage.app",
+  messagingSenderId: "619764545272",
+  appId: "1:619764545272:web:68d926a7cc13651bb3ad0d",
+  measurementId: "G-H8W2QX4R18"
+};
+
+const App = initializeApp(firebaseConfig);
+export default App;
